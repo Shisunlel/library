f6699a6436c2de72762f5d74756c7f2a90f8c1c0
Shisun <worlddominator007@gmail.com>
2021-05-14 21:48:19 +0700

    add localStorage

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".close");
 const form = document.querySelector("form");
 const bookContainer = document.querySelector("main");
+let localLibrary = localStorage.getItem("myLibrary");
 let exist = false;
 let i = 0;
-
-window.onload = resetForm;
-
 let myLibrary = [];
+
+const setLocal = (myLibrary) => {
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localLibrary = localStorage.getItem("myLibrary");
+};
 
 function Book(title, author, pageNumber, hasRead) {
   this.title = title;
@@ -20,7 +23,57 @@ function Book(title, author, pageNumber, hasRead) {
   };
 }
 
-function addBookToLibrary() {
+function addToGrid(e) {
+  const card = document.createElement("div");
+  const title = document.createElement("h1");
+  const author = document.createElement("h4");
+  const pageNumber = document.createElement("h5");
+  const bookAction = document.createElement("div");
+  const hasReadToggler = document.createElement("button");
+  const removeButton = document.createElement("button");
+
+  card.className = "book-content";
+  card.dataset.index = i++;
+  title.className = "book-title";
+  author.className = "book-author";
+  pageNumber.className = "book-page-number";
+  bookAction.className = "book-action";
+  hasReadToggler.className = "has-read-toggler";
+  removeButton.className = "remove-book";
+
+  title.innerText = e.title;
+  author.innerText = e.author;
+  pageNumber.innerText = e.pageNumber + " pages";
+  removeButton.innerText = "Remove";
+
+  hasReadToggler.addEventListener("click", hasReadToggle);
+  removeButton.addEventListener("click", removeBook);
+
+  e.hasRead
+    ? (hasReadToggler.innerText = "Has Read")
+    : (hasReadToggler.innerText = "Has Not Read");
+  e.hasRead
+    ? hasReadToggler.classList.remove("has-not")
+    : hasReadToggler.classList.add("has-not");
+
+  bookAction.append(hasReadToggler, removeButton);
+  card.append(title, author, pageNumber, bookAction);
+  bookContainer.append(card);
+}
+
+function addToView() {
+  bookContainer.innerHTML = "";
+  myLibrary.forEach((e) => {
+    addToGrid(e);
+  });
+}
+
+if (localLibrary != "") {
+  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  addToView();
+}
+
+const addBookToLibrary = () => {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pageNumber = document.querySelector("#pageNumber").value;
@@ -30,68 +83,27 @@ function addBookToLibrary() {
   });
   if (!exist) {
     myLibrary.push(new Book(title, author, pageNumber, hasRead));
+    setLocal(myLibrary);
     addToView();
   } else {
     alert("Cannot add duplicate");
   }
-}
+};
 
-function addToView() {
-  bookContainer.innerHTML = "";
-  myLibrary.forEach((e) => {
-    const card = document.createElement("div");
-    const title = document.createElement("h1");
-    const author = document.createElement("h4");
-    const pageNumber = document.createElement("h5");
-    const bookAction = document.createElement("div");
-    const hasReadToggler = document.createElement("button");
-    const removeButton = document.createElement("button");
-
-    card.className = "book-content";
-    card.dataset.index = i++;
-    title.className = "book-title";
-    author.className = "book-author";
-    pageNumber.className = "book-page-number";
-    bookAction.className = "book-action";
-    hasReadToggler.className = "has-read-toggler";
-    removeButton.className = "remove-book";
-    
-    title.innerText = e.title;
-    author.innerText = e.author;
-    pageNumber.innerText = e.pageNumber + " pages";
-    removeButton.innerText = "Remove";
-
-    hasReadToggler.addEventListener("click", hasReadToggle);
-    removeButton.addEventListener("click", removeBook);
-
-    e.hasRead
-      ? (hasReadToggler.innerText = "Has Read")
-      : (hasReadToggler.innerText = "Has Not Read");
-    e.hasRead
-      ? hasReadToggler.classList.remove("has-not")
-      : hasReadToggler.classList.add("has-not");
-
-
-    bookAction.append(hasReadToggler, removeButton);
-    card.append(title, author, pageNumber, bookAction);
-    bookContainer.append(card);
-  });
-}
-
-function showPopup() {
+const showPopup = () => {
   popup.classList.add("show");
   closeButton.classList.add("show");
-}
+};
 
-function hidePopup() {
+const hidePopup = () => {
   popup.classList.remove("show");
   closeButton.classList.remove("show");
-}
+};
 
-function resetForm() {
+const resetForm = () => {
   form.reset();
   i = 0;
-}
+};
 
 function hasReadToggle() {
   this.classList.toggle("has-not");
@@ -99,13 +111,19 @@ function hasReadToggle() {
     ? (this.innerText = "Has Read")
     : (this.innerText = "Has Not Read");
   const thisBook = myLibrary[this.parentNode.parentNode.dataset.index];
-  this.innerText === "Has Not Read" ? thisBook.hasRead = false : thisBook.hasRead = true;
+  this.innerText === "Has Not Read"
+    ? (thisBook.hasRead = false)
+    : (thisBook.hasRead = true);
+  setLocal(myLibrary);
 }
 
 function removeBook() {
   myLibrary = myLibrary.filter((e) => !e.title);
   this.parentNode.parentNode.remove();
+  setLocal(myLibrary);
 }
+
+window.onload = resetForm;
 
 addNewButton.addEventListener("click", showPopup);
 closeButton.addEventListener("click", () => {
@@ -119,10 +137,3 @@ form.addEventListener("submit", (e) => {
   hidePopup();
   resetForm();
 });
-
-// Book.prototype.print = function(){
-//     return this.title
-// }
-
-// const book = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not read yet')
-// console.info(book.info())
